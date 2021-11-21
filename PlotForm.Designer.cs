6a01a4b2f814eaa0f53695df2c032322b9a8889e
Francesco Bonicelli <cescobonic@gmail.com>
2021-11-21 11:17:58 +0100

    Added plot form

---
+++ PlotForm.Designer.cs
@@ -0,0 +1,60 @@
+﻿namespace SDRSharp.Plugin.SignalRecorder
+{
+    partial class PlotForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.TestLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // TestLabel
+            // 
+            this.TestLabel.AutoSize = true;
+            this.TestLabel.Location = new System.Drawing.Point(113, 211);
+            this.TestLabel.Name = "TestLabel";
+            this.TestLabel.Size = new System.Drawing.Size(72, 20);
+            this.TestLabel.TabIndex = 0;
+            this.TestLabel.Text = "FileName";
+            // 
+            // PlotForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.TestLabel);
+            this.Name = "PlotForm";
+            this.Text = "PlotForm";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label TestLabel;
+    }
+}
